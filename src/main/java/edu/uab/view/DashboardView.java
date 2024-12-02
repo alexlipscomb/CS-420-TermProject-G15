22feb530c591453ff35e93487ab8152fdb6efb7c
Ryan-UAB <ryandale@uab.edu>
2024-12-02 01:26:08 -0600

    feat: built out the dashboard in fxml file

--- src/main/java/edu/uab/view/DashboardView.java
+++ src/main/java/edu/uab/view/DashboardView.java
@@ -1,5 +1,11 @@
 package edu.uab.view;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class DashboardView {
   // TODO
 }
